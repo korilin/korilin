55fb9fb9ecb07ce7552a333a753c939f7b261577
korilin <korilin.dev@gmail.com>
2021-08-07 02:51:34 +0800

    Web APP

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 - 活在二次元，可爱即正义🌌
 - 技术宅，热爱开源，熬夜冠军🌛
 - 个人博客：<https://korilin.com/>
+- 即刻 id：korilin
+- 邮箱：korilin.dev@gmail.com
 
 ### 脑壳里装的是什么 💣
 
---
+++ app/app/src/main/java/com/korilin/app/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.korilin.app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.view.ViewCompat
+import com.korilin.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        webView = mainBinding.webview.apply {
+            loadUrl(BLOG_URL)
+            settings.javaScriptEnabled = true
+            setBackgroundColor(Color.WHITE)
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                    if (request?.url?.host == HOST) return false
+                    Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+                        startActivity(this)
+                    }
+                    return true
+                }
+            }
+        }
+
+        ViewCompat.getWindowInsetsController(mainBinding.root)
+
+        window.apply {
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            statusBarColor = Color.TRANSPARENT
+        }
+
+        setContentView(mainBinding.root)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
