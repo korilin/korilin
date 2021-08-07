90a1483b544d61ac498cb46f30746156d011edeb
korilin <korilin.dev@gmail.com>
2021-08-07 15:34:09 +0800

    添加浮动按钮
    - 添加浮动操作按钮
    - 链接复制按钮
    - 链接分享按钮

--- app/app/src/main/java/com/korilin/app/MainActivity.kt
+++ app/app/src/main/java/com/korilin/app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.korilin.app
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
@@ -12,13 +15,19 @@ import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.korilin.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var webView: WebView
+
+    private var optionIn = false
+
+    private lateinit var clipboardManager: ClipboardManager
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +46,53 @@ class MainActivity : AppCompatActivity() {
                         startActivity(this)
                     }
                     return true
+                }
+            }
+        }
+
+        clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        mainBinding.apply {
+
+            copy.apply {
+                hide()
+                setOnClickListener {
+                    clipboardManager.setPrimaryClip(
+                        ClipData.newPlainText("link", webView.url)
+                    )
+                    Toast.makeText(context, "链接已复制到剪切板", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            share.apply {
+                hide()
+                setOnClickListener {
+                    Intent().apply {
+                        action = Intent.ACTION_SEND
+                        type = "text/plain"
+                        putExtra(Intent.EXTRA_SUBJECT, "来自 Kori Lin 个人博客的链接")
+                        putExtra(Intent.EXTRA_TEXT, "${webview.title} ：${webview.url}")
+                    }.also {
+                        Intent.createChooser(it, "分享")
+                    }.apply {
+                        startActivity(this)
+                    }
+                }
+            }
+
+            option.apply {
+                show()
+                setOnClickListener {
+                    if (optionIn) {
+                        setImageResource(android.R.drawable.ic_dialog_dialer)
+                        copy.hide()
+                        share.hide()
+                    } else {
+                        setImageResource(android.R.drawable.ic_menu_close_clear_cancel)
+                        copy.show()
+                        share.show()
+                    }
+                    optionIn = !optionIn
                 }
             }
         }
